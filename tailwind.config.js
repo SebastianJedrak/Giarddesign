4cb1854ac599f4c67625a6a64da0fada1a287da7
SebastianJedrak <jedrak.sebastian@gmail.com>
2023-08-15 13:41:46 +0200

    added responsiveness to nav section

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./index.html"],
   theme: {
-    extend: {}
+    extend: {},
+    screens: {
+      '2xl': {'max': '1535px'},
+      'xl': {'max': '1279px'},
+      'lg': {'max': '1023px'},
+      'md': {'max': '767px'},
+      'sm': {'max': '639px'},
+    }
   },
   plugins: [],
 }
