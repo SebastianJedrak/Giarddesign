c6fee9efc06772d465596f5d85496492eea66d5c
SebastianJedrak <jedrak.sebastian@gmail.com>
2023-08-16 16:11:10 +0200

    animate masonry

--- src/script.js
+++ src/script.js
@@ -3,7 +3,8 @@ import Masonry from "masonry-layout";
 // MASONRY
 const masonryGrid = document.querySelector(".grid-masonry");
 const overlay = document.querySelector(".masonry-overlay");
-const masonryBtn = document.querySelector(".masonry-btn");
+const masonryBtnOpen = document.querySelector(".masonry-btn-open");
+const masonryBtnClose = document.querySelector(".masonry-btn-close");
 
 //Display
 window.addEventListener("load", () => {
@@ -13,8 +14,34 @@ window.addEventListener("load", () => {
   });
 });
 
-//Show / Hide
-masonryBtn.addEventListener("click", () => {
-  masonryGrid.classList.toggle("max-h-[40rem]");
+//Expand animation
+const expandKeyframes = [{ maxHeight: "40rem" }, { maxHeight: "250rem" }];
+
+const ExpandOptions = {
+  duration: 2000,
+  fill: "forwards",
+  easing: "cubic-bezier(1,0,.12,1)",
+};
+
+//Show
+masonryBtnOpen.addEventListener("click", () => {
+  masonryGrid.animate(expandKeyframes, ExpandOptions);
+
+  setTimeout(() => {
+    overlay.classList.toggle("h-full");
+    masonryBtnOpen.classList.toggle("hidden");
+    masonryBtnClose.classList.toggle("hidden");
+  }, 1000);
+});
+
+// Hide
+masonryBtnClose.addEventListener("click", () => {
+  masonryGrid.animate(expandKeyframes, {
+    ...ExpandOptions,
+    direction: "reverse",
+    duration: 500,
+  });
   overlay.classList.toggle("h-full");
+  masonryBtnOpen.classList.toggle("hidden");
+  masonryBtnClose.classList.toggle("hidden");
 });
